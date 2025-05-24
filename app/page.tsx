da5e3578a30015783e6466e70a7877c5b2c8525a
Mike <binarybardcode@gmail.com>
2025-05-24 16:37:25 +0200

    add routes

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-2xl underline'>Home</div>
   )
 }
 
-export default Home
+export default Home 
